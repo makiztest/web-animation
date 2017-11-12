8ec2d924423b0d3bceabe87b93eb78c11b56ee64
Mark Delos Reyes <makizworks@gmail.com>
2017-11-11 22:54:59 -0500

    duration

---
+++ assets/js/transition.js
@@ -0,0 +1,8 @@
+$("#trans-duration").on("click", function(e) {
+  // e.preventDefault();
+  $(".trans-dur").css({
+    "background": "steelblue",
+    "margin-right": "50%",
+    "color": "snow"
+  });
+});
